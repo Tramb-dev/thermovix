86a23eaf4cfef56160466d25219314d26d987bc4
Tramb-dev <83473808+Tramb-dev@users.noreply.github.com>
2021-05-26 15:29:14 +0200

    Create config.php

---
+++ src/config.php
@@ -0,0 +1,20 @@
+<?php
+/**************************************************************/
+/* config.php
+/* Fichier de configuration (BDD, Google etc).
+/**************************************************************/
+
+define('G_CLIENT_ID', 'xxx.apps.googleusercontent.com');
+define('G_CLIENT_SECRET', 'xxx');
+define('SQL_DB', 'xxx');
+define('SQL_NAME', 'xxx');
+define('SQL_LOGIN', 'xxx');
+define('SQL_PASS', 'xxx');
+define('SQL_PORT', 'xxx');
+define('ROOT', 'xxx');
+define('API_KEY', 'xxx');
+
+setlocale (LC_TIME, 'fr_FR','fra');
+date_default_timezone_set("Europe/Paris");
+mb_internal_encoding("UTF-8");
+?>
